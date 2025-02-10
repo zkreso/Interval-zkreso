a2d3d263765eb8661418e7354b9bbb42cbb8605c
zkreso <zkreso@gmail.com>
2025-02-10 23:09:19 +0100

    Implementation of left-bound interval

---
+++ src/main/java/no/kreso/implementations/LeftBoundInterval.java
@@ -0,0 +1,107 @@
+package no.kreso.implementations;
+
+import no.kreso.Interval;
+
+import java.time.LocalDate;
+
+/**
+ * Implementation of Interval for LocalDate that treats null as +infinity for both bounds.
+ * Lower bound is treated as inclusive and upper bound as exclusive (when not representing infinity).
+ * Unions between disjoint intervals are not supported and return the empty interval.
+ */
+public class LeftBoundInterval implements Interval<LocalDate> {
+
+    private static final LeftBoundInterval EMPTY_INTERVAL = new LeftBoundInterval(LocalDate.EPOCH, LocalDate.EPOCH);
+
+    private final LocalDate start;
+    private final LocalDate end;
+
+    private LeftBoundInterval(LocalDate start, LocalDate end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public static Interval<LocalDate> of(LocalDate start, LocalDate end) {
+        if (start == null || end != null && start.isAfter(end)) {
+            return EMPTY_INTERVAL;
+        }
+        return new LeftBoundInterval(start, end);
+    }
+
+    @Override
+    public LocalDate start() {
+        return start;
+    }
+
+    @Override
+    public LocalDate end() {
+        return end;
+    }
+
+    @Override
+    public boolean subsetOf(Interval<LocalDate> other) {
+        if (this.isEmpty()) {
+            return true;
+        }
+        if (other.isEmpty()) {
+            return this.isEmpty();
+        }
+        return compareTo(this.start, other.start()) >= 0 && compareTo(this.end, other.end()) <= 0;
+    }
+
+    private int compareTo(LocalDate fst, LocalDate snd) {
+        if (fst == null && snd == null) {
+            return 0;
+        }
+        if (fst == null ^ snd == null) {
+            return (fst == null) ? 1 : -1;
+        }
+        return fst.compareTo(snd);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return start == null || end != null && start.isEqual(end);
+    }
+
+    @Override
+    public Interval<LocalDate> intersection(Interval<LocalDate> other) {
+        if (this.isEmpty()) {
+            return this;
+        }
+        if (other.isEmpty()) {
+            return other;
+        }
+        return of(
+                max(start, other.start()),
+                min(end, other.end())
+        );
+    }
+
+    private LocalDate max(LocalDate fst, LocalDate snd) {
+        return compareTo(fst, snd) > 0 ? fst : snd;
+    }
+
+    private LocalDate min(LocalDate fst, LocalDate snd) {
+        return compareTo(fst, snd) < 0 ? fst : snd;
+    }
+
+    @Override
+    public Interval<LocalDate> union(Interval<LocalDate> other) {
+        if (this.isEmpty() || other.isEmpty()) {
+            return EMPTY_INTERVAL;
+        }
+        // If this ends before other begins,
+        boolean endsBeforeOtherStarts = this.end != null && compareTo(this.end, other.start()) < 0;
+        // or if this starts after other ends,
+        boolean startsAfterOtherEnds = this.start == null || compareTo(this.start, other.end()) > 0;
+        // then they are disjoint.
+        if (endsBeforeOtherStarts || startsAfterOtherEnds) {
+            return EMPTY_INTERVAL;
+        }
+        return of(
+                min(start, other.start()),
+                max(end, other.end())
+        );
+    }
+}
---
+++ src/test/java/no/kreso/implementations/LeftBoundIntervalTest.java
@@ -0,0 +1,155 @@
+package no.kreso.implementations;
+
+import no.kreso.Interval;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LeftBoundIntervalTest {
+
+    private final LocalDate feb5th = LocalDate.of(2025, 2, 5);
+    private final LocalDate feb10th = LocalDate.of(2025, 2, 10);
+    private final LocalDate feb11th = LocalDate.of(2025, 2, 11);
+    private final LocalDate feb20th = LocalDate.of(2025, 2, 20);
+
+    @Test
+    public void creation() {
+        Interval<LocalDate> interval = LeftBoundInterval.of(feb5th, feb10th);
+        assertTrue(interval.start().isEqual(feb5th));
+        assertTrue(interval.end().isEqual(feb10th));
+    }
+
+    @Test
+    public void creationWithNull() {
+        Interval<LocalDate> interval;
+
+        interval = LeftBoundInterval.of(null, feb10th);
+        assertTrue(interval.isEmpty());
+
+        interval = LeftBoundInterval.of(feb10th, null);
+        assertTrue(interval.start().isEqual(feb10th));
+        assertNull(interval.end());
+        assertFalse(interval.isEmpty());
+
+        interval = LeftBoundInterval.of(null, null);
+        assertTrue(interval.isEmpty());
+    }
+
+    @Test
+    public void creationWithReversedParameters() {
+        Interval<LocalDate> interval;
+
+        interval = LeftBoundInterval.of(feb10th, feb5th);
+        assertTrue(interval.isEmpty());
+    }
+
+    @Test
+    public void subsetOf() {
+        Interval<LocalDate> emptySet = LeftBoundInterval.of(feb5th, feb5th);
+        Interval<LocalDate> interval;
+
+        // 1. A set is always a subset of itself
+        // 2. The empty set is a subset of all other sets
+        // 3. The empty set has only the empty set as a subset
+        interval = LeftBoundInterval.of(feb5th, feb10th);
+        assertTrue(interval.subsetOf(interval));
+        assertTrue(emptySet.subsetOf(interval));
+        assertFalse(interval.subsetOf(emptySet));
+
+        interval = LeftBoundInterval.of(feb5th, null);
+        assertTrue(interval.subsetOf(interval));
+        assertTrue(emptySet.subsetOf(interval));
+        assertFalse(interval.subsetOf(emptySet));
+
+        interval = LeftBoundInterval.of(null, feb5th);
+        assertTrue(interval.subsetOf(interval));
+        assertTrue(emptySet.subsetOf(interval));
+        assertTrue(interval.subsetOf(emptySet));
+
+        interval = LeftBoundInterval.of(null, null);
+        assertTrue(interval.subsetOf(interval));
+        assertTrue(emptySet.subsetOf(interval));
+        assertTrue(interval.subsetOf(emptySet));
+
+        interval = LeftBoundInterval.of(feb20th, feb20th);
+        assertTrue(interval.subsetOf(interval));
+        assertTrue(emptySet.subsetOf(interval));
+        assertTrue(interval.subsetOf(emptySet));
+    }
+
+    @Test
+    public void union() {
+        Interval<LocalDate> interval;
+        Interval<LocalDate> other;
+        Interval<LocalDate> union;
+
+        interval = LeftBoundInterval.of(feb5th, feb10th);
+        other = LeftBoundInterval.of(feb10th, feb20th);
+        union = interval.union(other);
+        assertTrue(union.start().isEqual(feb5th));
+        assertTrue(union.end().isEqual(feb20th));
+
+        interval = LeftBoundInterval.of(feb5th, null);
+        other = LeftBoundInterval.of(feb10th, feb20th);
+        union = interval.union(other);
+        assertTrue(union.start().isEqual(feb5th));
+        assertNull(union.end());
+
+        interval = LeftBoundInterval.of(feb5th, feb10th);
+        other = LeftBoundInterval.of(feb10th, null);
+        union = interval.union(other);
+        assertTrue(union.start().isEqual(feb5th));
+        assertNull(union.end());
+    }
+
+    @Test
+    public void unionDisjoint() {
+        Interval<LocalDate> interval;
+        Interval<LocalDate> other;
+        Interval<LocalDate> union;
+
+        // Unions of disjoint intervals should return the empty interval
+        // Also make sure that it is exclusive
+        interval = LeftBoundInterval.of(feb5th, feb10th);
+        other = LeftBoundInterval.of(feb11th, feb20th);
+        union = interval.union(other);
+        assertTrue(union.isEmpty());
+
+        interval = LeftBoundInterval.of(feb5th, feb10th);
+        other = LeftBoundInterval.of(feb11th, null);
+        union = interval.union(other);
+        assertTrue(union.isEmpty());
+    }
+
+    @Test
+    public void intersection() {
+        Interval<LocalDate> interval;
+        Interval<LocalDate> other;
+        Interval<LocalDate> intersection;
+
+        interval = LeftBoundInterval.of(feb5th, feb11th);
+        other = LeftBoundInterval.of(feb10th, feb20th);
+        intersection = interval.intersection(other);
+        assertTrue(intersection.start().isEqual(feb10th));
+        assertTrue(intersection.end().isEqual(feb11th));
+
+        interval = LeftBoundInterval.of(null, feb11th);
+        other = LeftBoundInterval.of(feb10th, feb20th);
+        intersection = interval.intersection(other);
+        assertTrue(intersection.isEmpty());
+
+        interval = LeftBoundInterval.of(feb5th, feb11th);
+        other = LeftBoundInterval.of(feb10th, null);
+        intersection = interval.intersection(other);
+        assertTrue(intersection.start().isEqual(feb10th));
+        assertTrue(intersection.end().isEqual(feb11th));
+
+        interval = LeftBoundInterval.of(feb5th, null);
+        other = LeftBoundInterval.of(feb10th, feb20th);
+        intersection = interval.intersection(other);
+        assertTrue(intersection.start().isEqual(feb10th));
+        assertTrue(intersection.end().isEqual(feb20th));
+    }
+}
