ad11ea97acf8208a79cc8b6b314207da22c372ad
zkreso <zkreso@gmail.com>
2025-02-12 01:39:34 +0100

    Rework

--- src/test/java/no/kreso/implementations/LeftBoundIntervalTest.java
+++ src/test/java/no/kreso/implementations/LeftBoundIntervalTest.java
@@ -1,6 +1,5 @@
 package no.kreso.implementations;
 
-import no.kreso.Interval;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
@@ -10,155 +9,184 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class LeftBoundIntervalTest {
 
-    private final LocalDate feb5th = LocalDate.of(2025, 2, 5);
-    private final LocalDate feb10th = LocalDate.of(2025, 2, 10);
-    private final LocalDate feb11th = LocalDate.of(2025, 2, 11);
-    private final LocalDate feb20th = LocalDate.of(2025, 2, 20);
-
-    IntervalOperations<LocalDate> leftBond = new LeftBound<>(Comparator.<LocalDate>naturalOrder());
+    record Params<T>(IntervalOperations<T> leftBound, T five, T ten, T eleven, T twenty) { }
 
     @Test
-    public void creation() {
-        IntervalInterface<LocalDate> interval = leftBond.validate(feb5th, feb10th);
-        assertEquals(interval.start(), feb5th);
-        assertEquals(interval.end(), feb10th);
+    public void testDate() {
+        Params<LocalDate> params = new Params<>(new LeftBound<LocalDate>(Comparator.naturalOrder()),
+                LocalDate.of(2025, 2, 5),
+                LocalDate.of(2025, 2, 10),
+                LocalDate.of(2025, 2, 11),
+                LocalDate.of(2025, 2, 20));
+        runTests(params);
     }
 
     @Test
-    public void creationWithNull() {
-        IntervalInterface<LocalDate> interval;
+    public void testInteger() {
+        Params<Integer> params = new Params<>(new LeftBound<Integer>(Comparator.naturalOrder()),
+                5, 10, 11, 20);
+        runTests(params);
+    }
 
-        interval = leftBond.validate(null, feb10th);
-        assertTrue(leftBond.isEmpty(interval));
+    @Test
+    public void testIntegerReverse() {
+        Params<Integer> params = new Params<>(new LeftBound<Integer>(Comparator.reverseOrder()),
+                20, 11, 10, 5);
+        runTests(params);
+    }
 
-        interval = leftBond.validate(feb10th, null);
-        assertEquals(interval.start(), feb10th);
+    public <T> void runTests(Params<T> params) {
+        creation(params);
+        creationWithNull(params);
+        creationWithReversedParameters(params);
+        subsetOf(params);
+        union(params);
+        unionDisjoint(params);
+        intersection(params);
+    }
+
+    public <T> void creation(Params<T> params) {
+        IntervalOperations<T> bound = params.leftBound();
+        IntervalInterface<T> interval = bound.validate(params.five, params.ten);
+        assertEquals(interval.start(), params.five);
+        assertEquals(interval.end(), params.ten);
+    }
+
+    public <T> void creationWithNull(Params<T> params) {
+        IntervalOperations<T> bound = params.leftBound();
+        IntervalInterface<T> interval;
+
+        interval = bound.validate(null, params.ten);
+        assertTrue(bound.isEmpty(interval));
+
+        interval = bound.validate(params.ten, null);
+        assertEquals(interval.start(), params.ten);
         assertNull(interval.end());
-        assertFalse(leftBond.isEmpty(interval));
+        assertFalse(bound.isEmpty(interval));
 
-        interval = leftBond.validate(null, null);
-        assertTrue(leftBond.isEmpty(interval));
+        interval = bound.validate(null, null);
+        assertTrue(bound.isEmpty(interval));
     }
 
-    @Test
-    public void creationWithReversedParameters() {
-        IntervalInterface<LocalDate> interval;
+    public <T> void creationWithReversedParameters(Params<T> params) {
+        IntervalOperations<T> bound = params.leftBound();
+        IntervalInterface<T> interval;
 
-        interval = leftBond.validate(feb10th, feb5th);
-        assertTrue(leftBond.isEmpty(interval));
+        interval = bound.validate(params.ten, params.five);
+        assertTrue(bound.isEmpty(interval));
     }
 
-    @Test
-    public void subsetOf() {
-        IntervalInterface<LocalDate> emptySet = leftBond.validate(feb5th, feb5th);
-        IntervalInterface<LocalDate> interval;
+    public <T> void subsetOf(Params<T> params) {
+        IntervalOperations<T> bound = params.leftBound();
+        IntervalInterface<T> emptySet = bound.validate(params.five, params.five);
+        IntervalInterface<T> interval;
 
         // 1. A set is always a subset of itself
         // 2. The empty set is a subset of all other sets
         // 3. The empty set has only the empty set as a subset
-        interval = leftBond.validate(feb5th, feb10th);
-        assertTrue(leftBond.subsetOf(interval, interval));
-        assertTrue(leftBond.subsetOf(emptySet, interval));
-        assertFalse(leftBond.subsetOf(interval, emptySet));
+        interval = bound.validate(params.five, params.ten);
+        assertTrue(bound.subsetOf(interval, interval));
+        assertTrue(bound.subsetOf(emptySet, interval));
+        assertFalse(bound.subsetOf(interval, emptySet));
 
-        interval = leftBond.validate(feb5th, null);
-        assertTrue(leftBond.subsetOf(interval, interval));
-        assertTrue(leftBond.subsetOf(emptySet, interval));
-        assertFalse(leftBond.subsetOf(interval, emptySet));
+        interval = bound.validate(params.five, null);
+        assertTrue(bound.subsetOf(interval, interval));
+        assertTrue(bound.subsetOf(emptySet, interval));
+        assertFalse(bound.subsetOf(interval, emptySet));
 
-        interval = leftBond.validate(null, feb5th);
-        assertTrue(leftBond.subsetOf(interval, interval));
-        assertTrue(leftBond.subsetOf(emptySet, interval));
-        assertTrue(leftBond.subsetOf(interval, emptySet));
+        interval = bound.validate(null, params.five);
+        assertTrue(bound.subsetOf(interval, interval));
+        assertTrue(bound.subsetOf(emptySet, interval));
+        assertTrue(bound.subsetOf(interval, emptySet));
 
-        interval = leftBond.validate(null, null);
-        assertTrue(leftBond.subsetOf(interval, interval));
-        assertTrue(leftBond.subsetOf(emptySet, interval));
-        assertTrue(leftBond.subsetOf(interval, emptySet));
+        interval = bound.validate(null, null);
+        assertTrue(bound.subsetOf(interval, interval));
+        assertTrue(bound.subsetOf(emptySet, interval));
+        assertTrue(bound.subsetOf(interval, emptySet));
 
-        interval = leftBond.validate(feb20th, feb20th);
-        assertTrue(leftBond.subsetOf(interval, interval));
-        assertTrue(leftBond.subsetOf(emptySet, interval));
-        assertTrue(leftBond.subsetOf(interval, emptySet));
+        interval = bound.validate(params.twenty, params.twenty);
+        assertTrue(bound.subsetOf(interval, interval));
+        assertTrue(bound.subsetOf(emptySet, interval));
+        assertTrue(bound.subsetOf(interval, emptySet));
     }
 
-    @Test
-    public void union() {
-        IntervalInterface<LocalDate> interval;
-        IntervalInterface<LocalDate> other;
-        IntervalInterface<LocalDate> union;
+    public <T> void union(Params<T> params) {
+        IntervalOperations<T> bound = params.leftBound();
+        IntervalInterface<T> interval;
+        IntervalInterface<T> other;
+        IntervalInterface<T> union;
 
-        interval = leftBond.validate(feb5th, feb10th);
-        other = leftBond.validate(feb10th, feb20th);
-        union = leftBond.union(interval, other);
-        assertEquals(union.start(), feb5th);
-        assertEquals(union.end(), feb20th);
+        interval = bound.validate(params.five, params.ten);
+        other = bound.validate(params.ten, params.twenty);
+        union = bound.union(interval, other);
+        assertEquals(union.start(), params.five);
+        assertEquals(union.end(), params.twenty);
 
-        interval = leftBond.validate(feb5th, null);
-        other = leftBond.validate(feb10th, feb20th);
-        union = leftBond.union(interval, other);
-        assertEquals(union.start(), feb5th);
+        interval = bound.validate(params.five, null);
+        other = bound.validate(params.ten, params.twenty);
+        union = bound.union(interval, other);
+        assertEquals(union.start(), params.five);
         assertNull(union.end());
 
-        interval = leftBond.validate(feb5th, feb10th);
-        other = leftBond.validate(feb10th, null);
-        union = leftBond.union(interval, other);
-        assertEquals(union.start(), feb5th);
+        interval = bound.validate(params.five, params.ten);
+        other = bound.validate(params.ten, null);
+        union = bound.union(interval, other);
+        assertEquals(union.start(), params.five);
         assertNull(union.end());
 
-        interval = leftBond.validate(feb5th, feb5th);
-        other = leftBond.validate(feb10th, null);
-        union = leftBond.union(interval, other);
-        assertEquals(union.start(), feb10th);
+        interval = bound.validate(params.five, params.five);
+        other = bound.validate(params.ten, null);
+        union = bound.union(interval, other);
+        assertEquals(union.start(), params.ten);
         assertNull(union.end());
     }
 
-    @Test
-    public void unionDisjoint() {
-        IntervalInterface<LocalDate> interval;
-        IntervalInterface<LocalDate> other;
-        IntervalInterface<LocalDate> union;
+    public <T> void unionDisjoint(Params<T> params) {
+        IntervalOperations<T> bound = params.leftBound();
+        IntervalInterface<T> interval;
+        IntervalInterface<T> other;
+        IntervalInterface<T> union;
 
         // Unions of disjoint intervals should return the empty interval
         // Also make sure that it is exclusive
-        interval = leftBond.validate(feb5th, feb10th);
-        other = leftBond.validate(feb11th, feb20th);
-        union = leftBond.union(interval, other);
-        assertTrue(leftBond.isEmpty(union));
+        interval = bound.validate(params.five, params.ten);
+        other = bound.validate(params.eleven, params.twenty);
+        union = bound.union(interval, other);
+        assertTrue(bound.isEmpty(union));
 
-        interval = leftBond.validate(feb5th, feb10th);
-        other = leftBond.validate(feb11th, null);
-        union = leftBond.union(interval, other);
-        assertTrue(leftBond.isEmpty(union));
+        interval = bound.validate(params.five, params.ten);
+        other = bound.validate(params.eleven, null);
+        union = bound.union(interval, other);
+        assertTrue(bound.isEmpty(union));
     }
 
-    @Test
-    public void intersection() {
-        IntervalInterface<LocalDate> interval;
-        IntervalInterface<LocalDate> other;
-        IntervalInterface<LocalDate> intersection;
+    public <T> void intersection(Params<T> params) {
+        IntervalOperations<T> bound = params.leftBound();
+        IntervalInterface<T> interval;
+        IntervalInterface<T> other;
+        IntervalInterface<T> intersection;
 
-        interval = leftBond.validate(feb5th, feb11th);
-        other = leftBond.validate(feb10th, feb20th);
-        intersection = leftBond.intersection(interval, other);
-        assertEquals(intersection.start(), feb10th);
-        assertEquals(intersection.end(), feb11th);
+        interval = bound.validate(params.five, params.eleven);
+        other = bound.validate(params.ten, params.twenty);
+        intersection = bound.intersection(interval, other);
+        assertEquals(intersection.start(), params.ten);
+        assertEquals(intersection.end(), params.eleven);
 
-        interval = leftBond.validate(null, feb11th);
-        other = leftBond.validate(feb10th, feb20th);
-        intersection = leftBond.intersection(interval, other);
-        assertTrue(leftBond.isEmpty(intersection));
+        interval = bound.validate(null, params.eleven);
+        other = bound.validate(params.ten, params.twenty);
+        intersection = bound.intersection(interval, other);
+        assertTrue(bound.isEmpty(intersection));
 
-        interval = leftBond.validate(feb5th, feb11th);
-        other = leftBond.validate(feb10th, null);
-        intersection = leftBond.intersection(interval, other);
-        assertEquals(intersection.start(), feb10th);
-        assertEquals(intersection.end(), feb11th);
+        interval = bound.validate(params.five, params.eleven);
+        other = bound.validate(params.ten, null);
+        intersection = bound.intersection(interval, other);
+        assertEquals(intersection.start(), params.ten);
+        assertEquals(intersection.end(), params.eleven);
 
-        interval = leftBond.validate(feb5th, null);
-        other = leftBond.validate(feb10th, feb20th);
-        intersection = leftBond.intersection(interval, other);
-        assertEquals(intersection.start(), feb10th);
-        assertEquals(intersection.end(), feb20th);
+        interval = bound.validate(params.five, null);
+        other = bound.validate(params.ten, params.twenty);
+        intersection = bound.intersection(interval, other);
+        assertEquals(intersection.start(), params.ten);
+        assertEquals(intersection.end(), params.twenty);
     }
 }
